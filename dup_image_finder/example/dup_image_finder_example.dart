74dcae7ae30d341ab9bf171cdd5d23f7c471f355
RequieMa <mzo_7071@qq.com>
2025-03-13 22:58:20 +0800

    try to use opencv_dart, need to convert all to opencv. Currently need to get dartcv.dll

--- dup_image_finder/example/dup_image_finder_example.dart
+++ dup_image_finder/example/dup_image_finder_example.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
 import 'package:dup_image_finder/dup_image_finder.dart';
+import 'package:opencv_dart/opencv_dart.dart' as cv;
 
 void main() {
   var awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
+  var image = cv.imread('example/example_img/cat.png', flags: cv.IMREAD_GRAYSCALE);
+  var imageFloat = image.convertTo(cv.MatType.CV_32FC1);
+  var dct = cv.dct(imageFloat);
+  var idct = cv.idct(dct);
+  cv.imwrite('example/example_img/reconstructed_image.png', idct);
 }
